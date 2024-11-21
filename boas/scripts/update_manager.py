e3cba5c107b6221e7c1bfb6a75ccc6787f619d73
DaTrueMeme <97398378+DaTrueMeme@users.noreply.github.com>
2024-11-20 23:58:00 -0600

    Update 0.0.3
    Update Manager Fix
       Version 0.0.3
    
    --- Update Manager Updates ---
    • Fix problem with update manager not downloading files

--- boas/scripts/update_manager.py
+++ boas/scripts/update_manager.py
@@ -1,28 +1,47 @@
 import requests
 import os
+import shutil
 from boas.scripts.functions import *
 
-def update(folder_name="boas"):
-    repo = grabData("settings")["repository"]
+def permissionFix(func, path, exc_info):
+    import stat
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
 
+def downloadContent(repo, folder_name, temp_folder_name):
     url = f'https://api.github.com/repos/{repo}/contents/{folder_name}'
     response = requests.get(url)
     response.raise_for_status()
-
     data = response.json()
 
-    os.makedirs(folder_name, exist_ok=True)
-
     for item in data:
-        if item['type'] == "file":
+        if item['type'] == 'file':
             file_url = item['download_url']
             file_name = item['name']
-            file_path = os.path.join(folder_name, file_name)
+            file_path = os.path.join(temp_folder_name, item['path'])
 
-            response = requests.get(file_url)
-            response.raise_for_status()
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
+            file_response = requests.get(file_url)
+            file_response.raise_for_status()
             with open(file_path, 'wb') as f:
-                f.write(response.content)
+                f.write(file_response.content)
+        elif item['type'] == 'dir':
+            downloadContent(repo, item['path'], temp_folder_name)
+
+def update(folder_name="boas"):
+    temp_folder_name = f'temp_{folder_name}'
+    repo = grabData("settings")["repository"]
+
+    os.makedirs(temp_folder_name, exist_ok=True)
+
+    downloadContent(repo, folder_name, temp_folder_name)
+
+    try:
+        shutil.rmtree(folder_name, onerror=permissionFix)
+    except Exception as e:
+        print(f"An error occurred while deleting the old folder: {e}")
+
+    os.rename(temp_folder_name, folder_name)
 
     print("Boas update successfully installed!\nRestart to use it.")
